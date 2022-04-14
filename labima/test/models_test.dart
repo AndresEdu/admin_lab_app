5ace88a506537affda65c29c99954880fdb65382
Luis Talavera <luisfertalavera15@gmail.com>
2022-04-14 12:45:41 -0500

    Agregar test del constructor fromJson de Alumno

--- labima/test/models_test.dart
+++ labima/test/models_test.dart
@@ -4,7 +4,7 @@ import 'package:labima/models/alumno.dart';
 void main() {
   group('Alumno', () {
     test('Generar map a partir de Alumno', () {
-      Alumno alumno = const Alumno(
+      Alumno alumno = Alumno(
           aid: "ax1914",
           noControl: "19141205",
           nombre: "Fernando",
@@ -27,6 +27,46 @@ void main() {
       json["esDeudor"] = false;
 
       expect(alumno.toJson(), json);
+    });
+
+    test('Construir Alumno a partir de map', () {
+      Map<String, dynamic> json = <String, dynamic>{};
+      json["aid"] = "ax1914";
+      json["noControl"] = "19141205";
+      json["nombre"] = "Fernando";
+      json["apellidoPaterno"] = "Rivera";
+      json["apellidoMaterno"] = "Mendez";
+      json["carrera"] = "ISC";
+      json["contrasegna"] = "123456";
+      json["correo"] = "l19141205@queretaro.tecnm.mx";
+      json["esDeudor"] = false;
+
+      Alumno alumno1 = Alumno.fromJson(json);
+
+      Alumno alumno2 = Alumno(
+          aid: "ax1914",
+          noControl: "19141205",
+          nombre: "Fernando",
+          apellidoPaterno: "Rivera",
+          apellidoMaterno: "Mendez",
+          carrera: "ISC",
+          contrasegna: "123456",
+          correo: "l19141205@queretaro.tecnm.mx",
+          esDeudor: false);
+
+      Alumno alumno3 = Alumno(
+          aid: "ax1928", // ID diferente
+          noControl: "19141205",
+          nombre: "Fernando",
+          apellidoPaterno: "Rivera",
+          apellidoMaterno: "Mendez",
+          carrera: "ISC",
+          contrasegna: "123456",
+          correo: "l19141205@queretaro.tecnm.mx",
+          esDeudor: true); // esDeudor diferente
+
+      expect(alumno1 == alumno2, true);
+      expect(alumno1 == alumno3, false);
     });
   });
 }
