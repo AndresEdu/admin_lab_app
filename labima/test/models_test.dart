28dc40b5a39997844bc0ac48f0cca667ce9f524f
BigMama <mane.ariasa@gmail.com>
2022-04-16 20:51:54 -0500

    Generar test de encargado (requiere resideñar horas servicio)

--- labima/test/models_test.dart
+++ labima/test/models_test.dart
@@ -102,4 +102,85 @@ void main() {
       expect(c1, isNot(equals(c3)));
     });
   });
+
+  group('Encargado', () {
+    test('Generar map a partir de Encargado', () {
+      Encargado encargado = Encargado(
+        encid: "ax1914",
+        noControl: "19141205",
+        nombre: "Fernando",
+        apellidoPaterno: "Rivera",
+        apellidoMaterno: "Mendez",
+        carrera: "ISC",
+        contrasegna: "123456",
+        correo: "l19141205@queretaro.tecnm.mx",
+        hrInicio: DateTime.now(),
+        hrFinal: DateTime.utc(1989, DateTime.november, 9),
+        hrTotal: 4,
+      );
+
+      Map<String, dynamic> json = <String, dynamic>{};
+      json["encid"] = "ax1914";
+      json["noControl"] = "19141205";
+      json["nombre"] = "Fernando";
+      json["apellidoPaterno"] = "Rivera";
+      json["apellidoMaterno"] = "Mendez";
+      json["carrera"] = "ISC";
+      json["contrasegna"] = "123456";
+      json["correo"] = "l19141205@queretaro.tecnm.mx";
+      json["hrInicio"] = DateTime.now();
+      json["hrFinal"] = DateTime.utc(1989, DateTime.november, 9);
+      json["hrTotal"] = 4;
+
+      expect(encargado.toJson(), equals(json));
+    });
+
+    test('Construir Encargado a partir de map', () {
+      Map<String, dynamic> json = <String, dynamic>{};
+      json["encid"] = "ax1914";
+      json["noControl"] = "19141205";
+      json["nombre"] = "Fernando";
+      json["apellidoPaterno"] = "Rivera";
+      json["apellidoMaterno"] = "Mendez";
+      json["carrera"] = "ISC";
+      json["contrasegna"] = "123456";
+      json["correo"] = "l19141205@queretaro.tecnm.mx";
+      json["hrInicio"] = DateTime.now();
+      json["hrFinal"] = DateTime.utc(1989, DateTime.november, 9);
+      json["hrTotal"] = 4;
+
+      Encargado encargado1 = Encargado.fromJson(json);
+
+      Encargado encargado2 = Encargado(
+        encid: "ax1914",
+        noControl: "19141205",
+        nombre: "Fernando",
+        apellidoPaterno: "Rivera",
+        apellidoMaterno: "Mendez",
+        carrera: "ISC",
+        contrasegna: "123456",
+        correo: "l19141205@queretaro.tecnm.mx",
+        hrInicio: DateTime.now(),
+        hrFinal: DateTime.utc(1989, DateTime.november, 9),
+        hrTotal: 4,
+      );
+
+      Encargado encargado3 = Encargado(
+        encid: "ax1928", // ID diferente
+        noControl: "19141205",
+        nombre: "Fernando",
+        apellidoPaterno: "Rivera",
+        apellidoMaterno: "Mendez",
+        carrera: "ISC",
+        contrasegna: "123456",
+        correo: "l19141205@queretaro.tecnm.mx",
+        hrInicio: DateTime.utc(1989, DateTime.november, 9),
+        hrFinal: DateTime.now(),
+        hrTotal: 6,
+      ); //horas diferentes
+
+      expect(encargado1, equals(encargado2));
+      expect(encargado1, isNot(equals(encargado3)));
+    });
+  });
 }
