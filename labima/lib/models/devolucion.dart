032a74406141e9a12a8ec673d60a9f5c6a3aa63c
Luis Talavera <luisfertalavera15@gmail.com>
2022-04-29 12:12:38 -0500

    Modelos deudores, deuda, prestamo y devolucion

--- labima/lib/models/devolucion.dart
+++ labima/lib/models/devolucion.dart
@@ -1,10 +1,10 @@
 /*
-  ID_prestamo: id_prestamo int 
+  ID_Devolucion: id_Devolucion int 
   encargado: eid int 
   solicitante: sid int
-  tipoSolicitante: int // Nota: 1 - Alumno, 2 - Profesor, 3 - Investigador
+  prestamo: int // Nota: 1 - Alumno, 2 - Profesor, 3 - Investigador
   
-  maquina: mid // Si el prestamo no requiere maquina este valor es nulo
+  maquina: mid // Si el Devolucion no requiere maquina este valor es nulo
   piezas: map<int, List<int>(2)> 
   // El primer int es el id de la pieza, el primer int
   // de la lista es la cantidad esperada y el segundo es la cantidad
@@ -13,3 +13,46 @@
   fechaDevolucion: List<DateTime>
   devolucionCompleta: bool
 */
+
+import 'package:labima/models/prestamo.dart';
+import 'dart:ui' show hashValues, hashList;
+import 'package:json_annotation/json_annotation.dart';
+part 'devolucion.g.dart';
+
+@JsonSerializable()
+class Devolucion {
+  @JsonKey(required: true)
+  final String dvid;
+  final String encargadoId;
+  final Prestamo prestamo;
+  final List<Map<String, int>> devuelvePiezas; // String idPieza, Int: cantidad
+  final DateTime fecha;
+  final bool completo;
+
+  Devolucion({
+    required this.dvid,
+    required this.encargadoId,
+    required this.prestamo,
+    required this.devuelvePiezas,
+    required this.fecha,
+    required this.completo,
+  });
+  factory Devolucion.fromJson(Map<String, dynamic> json) =>
+      _$DevolucionFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DevolucionToJson(this);
+
+  @override
+  operator ==(other) =>
+      other is Devolucion &&
+      other.dvid == dvid &&
+      other.encargadoId == encargadoId &&
+      other.devuelvePiezas == devuelvePiezas &&
+      other.prestamo == prestamo &&
+      other.completo == completo &&
+      other.fecha == fecha;
+
+  @override
+  int get hashCode => hashValues(
+      dvid, encargadoId, prestamo, completo, fecha, hashList(devuelvePiezas));
+}
--- labima/lib/models/models.dart
+++ labima/lib/models/models.dart
@@ -1,11 +1,12 @@
 export 'package:labima/models/alumno.dart';
 export 'package:labima/models/carrera.dart';
+export 'package:labima/models/deudas.dart';
 export 'package:labima/models/deudores.dart';
 export 'package:labima/models/devolucion.dart';
 export 'package:labima/models/encargado.dart';
 export 'package:labima/models/investigador.dart';
-export 'package:labima/models/maquina.dart';
 export 'package:labima/models/pieza.dart';
-export 'package:labima/models/prestamo.dart';
-export 'package:labima/models/profesor.dart';
+export 'package:labima/models/investigador.dart';
+export 'package:labima/models/maquina.dart';
 export 'package:labima/models/ubicacion.dart';
+export 'package:labima/models/profesor.dart';
