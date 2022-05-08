a9f4f7f783998573daa9b869210bee60722d47c1
Emmanuel Arias <mane.ariasa@gmail.com>
2022-05-08 18:53:27 -0500

    Test de deudas, prestamos, falta revisar deudores

--- labima/test/models_test.dart
+++ labima/test/models_test.dart
@@ -833,4 +833,186 @@ void main() {
       expect(deudores, equals(deudores2));
     });
   });
+
+  /*group("Devolucion", () {
+    test("Construir un map a partir de una devolucion", () {
+      Prestamo prestamo1 = Prestamo(
+        prid: "123",
+        encargadoId: "ax1914",
+        solicitantesIds: ["ax1916", "ax1917", "ax1918"],
+        tipoSolicitante: 1,
+        maquinaId: "maquina1",
+        prestaPiezas: [
+          {"pieza1": 5},
+          {"pieza2": 5},
+          {"pieza3": 5}
+        ], // String idPieza, Int: cantidad
+        fecha: DateTime(2022, 04, 21, 9, 0, 0),
+        horaInicial: DateTime(2022, 04, 21, 10, 0, 0),
+        horaLimite: DateTime(2022, 04, 21, 14, 0, 0),
+      );
+      Map<String, dynamic> json1 = <String, dynamic>{};
+      json1["prid"] = "123";
+      json1["encargadoId"] = "ax1914";
+      json1["solicitantesIds"] = ["ax1916", "ax1917", "ax1918"];
+      json1["tipoSolicitante"] = 1;
+      json1["maquinaId"] = "maquina1";
+      json1["prestaPiezas"] = [
+        {"pieza1": 5},
+        {"pieza2": 5},
+        {"pieza3": 5}
+      ];
+      json1["fecha"] = DateTime(2022, 04, 21, 9, 0, 0).toIso8601String();
+      json1["horaInicial"] = DateTime(2022, 04, 21, 10, 0, 0).toIso8601String();
+      json1["horaLimite"] = DateTime(2022, 04, 21, 14, 0, 0).toIso8601String();
+
+      Prestamo prestamo = Prestamo.fromJson(json1);
+
+      Devolucion devolucion = Devolucion(
+        dvid: "devolucion1",
+        encargadoId: "ax1914",
+        prestamo: prestamo1,
+        devuelvePiezas: [
+          {"pieza1": 5},
+          {"pieza2": 5},
+          {"pieza3": 5}
+        ], // String idPieza, Int: cantidad
+        fecha: DateTime(2022, 04, 21, 9, 0, 0),
+        completo: true,
+      );
+
+      Map<String, dynamic> json = <String, dynamic>{};
+      json["dvid"] = "devolucion1";
+      json["encargadoId"] = "ax1914";
+      json["prestamo"] = prestamo;
+      json["devuelvePiezas"] = [
+        {"pieza1": 5},
+        {"pieza2": 5},
+        {"pieza3": 5}
+      ];
+      json["fecha"] = DateTime(2022, 04, 21, 9, 0, 0).toIso8601String();
+      json["completo"] = true;
+
+      expect(devolucion.toJson(), equals(json));
+    });
+  });*/
+
+  group("Prestamo", () {
+    test("Construir un map a partir de Prestamo", () {
+      Prestamo prestamo = Prestamo(
+        prid: "123",
+        encargadoId: "ax1914",
+        solicitantesIds: ["ax1916", "ax1917", "ax1918"],
+        tipoSolicitante: 1,
+        maquinaId: "maquina1",
+        prestaPiezas: [
+          {"pieza1": 5},
+          {"pieza2": 5},
+          {"pieza3": 5}
+        ], // String idPieza, Int: cantidad
+        fecha: DateTime(2022, 04, 21, 9, 0, 0),
+        horaInicial: DateTime(2022, 04, 21, 10, 0, 0),
+        horaLimite: DateTime(2022, 04, 21, 14, 0, 0),
+      );
+
+      Map<String, dynamic> json = <String, dynamic>{};
+      json["prid"] = "123";
+      json["encargadoId"] = "ax1914";
+      json["solicitantesIds"] = ["ax1916", "ax1917", "ax1918"];
+      json["tipoSolicitante"] = 1;
+      json["maquinaId"] = "maquina1";
+      json["prestaPiezas"] = [
+        {"pieza1": 5},
+        {"pieza2": 5},
+        {"pieza3": 5}
+      ];
+      json["fecha"] = DateTime(2022, 04, 21, 9, 0, 0).toIso8601String();
+      json["horaInicial"] = DateTime(2022, 04, 21, 10, 0, 0).toIso8601String();
+      json["horaLimite"] = DateTime(2022, 04, 21, 14, 0, 0).toIso8601String();
+
+      expect(prestamo.toJson(), equals(json));
+    });
+
+    test("Construir un Prestamo a partir de map", () {
+      Map<String, dynamic> json = <String, dynamic>{};
+      json["prid"] = "123";
+      json["encargadoId"] = "ax1914";
+      json["solicitantesIds"] = ["ax1916", "ax1917", "ax1918"];
+      json["tipoSolicitante"] = 1;
+      json["maquinaId"] = "maquina1";
+      json["prestaPiezas"] = [
+        {"pieza1": 5},
+        {"pieza2": 5},
+        {"pieza3": 5}
+      ];
+      json["fecha"] = DateTime(2022, 04, 21, 9, 0, 0).toIso8601String();
+      json["horaInicial"] = DateTime(2022, 04, 21, 10, 0, 0).toIso8601String();
+      json["horaLimite"] = DateTime(2022, 04, 21, 14, 0, 0).toIso8601String();
+
+      Prestamo prestamo = Prestamo.fromJson(json);
+
+      Prestamo prestamo2 = Prestamo(
+        prid: "123",
+        encargadoId: "ax1914",
+        solicitantesIds: ["ax1916", "ax1917", "ax1918"],
+        tipoSolicitante: 1,
+        maquinaId: "maquina1",
+        prestaPiezas: [
+          {"pieza1": 5},
+          {"pieza2": 5},
+          {"pieza3": 5}
+        ], // String idPieza, Int: cantidad
+        fecha: DateTime(2022, 04, 21, 9, 0, 0),
+        horaInicial: DateTime(2022, 04, 21, 10, 0, 0),
+        horaLimite: DateTime(2022, 04, 21, 14, 0, 0),
+      );
+      expect(prestamo, equals(prestamo2));
+    });
+  });
+
+  group("Deuda", () {
+    test("Construir un map a partir de una Deuda", () {
+      Deuda deuda = Deuda(
+        ddid: "123",
+        debePiezas: [
+          {"pieza1": 5},
+          {"pieza2": 5},
+          {"pieza3": 5}
+        ],
+      );
+
+      Map<String, dynamic> json = <String, dynamic>{};
+      json["ddid"] = "123";
+      json["debePiezas"] = [
+        {"pieza1": 5},
+        {"pieza2": 5},
+        {"pieza3": 5}
+      ];
+
+      expect(deuda.toJson(), equals(json));
+    });
+
+    test("Construir una Deuda a partir de un map", () {
+      Map<String, dynamic> json = <String, dynamic>{};
+      json["ddid"] = "123";
+      json["debePiezas"] = [
+        {"pieza1": 5},
+        {"pieza2": 5},
+        {"pieza3": 5}
+      ];
+
+      Deuda deuda = Deuda.fromJson(json);
+
+      Deuda deuda1 = Deuda(
+        ddid: "123",
+        debePiezas: [
+          {"pieza1": 5},
+          {"pieza2": 5},
+          {"pieza3": 5}
+        ],
+      );
+
+      expect(deuda, equals(deuda1));
+    });
+  });
 }
